c1047cf79cc94eeaaad13a2793e8d3c0190d61b0
Jesse <dashmoment1017@gmail.com>
2018-03-19 17:15:34 +0800

    finish satacked ensemble model

--- stacked_regrssion/main.py
+++ stacked_regrssion/main.py
@@ -47,6 +47,22 @@ print('Train set head 5:',train.head(5))
 print("Train set:", train.shape)
 print("Test set:", test.shape)
 
+#check the numbers of samples and features
+print("The train data size before dropping Id feature is : {} ".format(train.shape))
+print("The test data size before dropping Id feature is : {} ".format(test.shape))
+
+#Save the 'Id' column
+train_ID = train['Id']
+test_ID = test['Id']
+
+#Now drop the  'Id' colum since it's unnecessary for  the prediction process.
+train.drop("Id", axis = 1, inplace = True)
+test.drop("Id", axis = 1, inplace = True)
+
+#check again the data size after dropping the 'Id' variable
+print("\nThe train data size after dropping Id feature is : {} ".format(train.shape)) 
+print("The test data size after dropping Id feature is : {} ".format(test.shape))
+
 #Data prerpocessing
 
 #Find outlier
@@ -124,7 +140,7 @@ fig = plt.figure()
 res = stats.probplot(train['SalePrice_log'], plot=plt)
 plt.show()
 
-y_train = train.SalePrice.values
+y_train = train.SalePrice_log.values
 all_data = pd.concat((train, test)).reset_index(drop=True)
 all_data.drop(['SalePrice', 'SalePrice_log'], axis=1, inplace=True)
 print(all_data.shape)
@@ -328,7 +344,208 @@ test = all_data[ntrain:]
 ####################
 #9. Modeling 
 ####################
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import lightgbm as lgb
 
+
+#Validation function
+n_folds = 5
+
+def rmsle_cv(model):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
+    rmse= np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
+    return(rmse)
+
+
+#LASSO Regression 
+lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+#Elastic Net Regression
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+#Kernel Ridge Regression
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+#Gradient Boosting Regression
+Boost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10, 
+                                   loss='huber', random_state =5)
+#XGBoost
+model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
+                             learning_rate=0.05, max_depth=3, 
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state =7, nthread = -1)
+
+#LightGBM
+model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                              learning_rate=0.05, n_estimators=720,
+                              max_bin = 55, bagging_fraction = 0.8,
+                              bagging_freq = 5, feature_fraction = 0.2319,
+                              feature_fraction_seed=9, bagging_seed=9,
+                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+
+#Save to submission files
+
+def rmsle(y, y_pred):
+    return np.sqrt(mean_squared_error(y, y_pred))
+
+def submission(pred, model, test_ID=test_ID):
+    sub = pd.DataFrame()
+    sub['Id'] = test_ID
+    sub['SalePrice'] = pred
+    save_filename = 'submission_' + model + '.csv' 
+    sub.to_csv(save_filename,index=False)
+    print(pred)
+    
+
+def fit_model_and_submission(model, model_name, x_train = train, y_train = y_train, x_test=test, test_ID=test_ID):
+    model.fit(x_train, y_train)
+    pred_train = model.predict(x_train)
+    pred_test = model.predict(x_test)
+    rmse = rmsle(y_train, pred_train)
+    print("rmse_",model_name," : ", rmse)
+    submission(np.expm1(pred_test), model_name)
+   
+    
+
+
+score = rmsle_cv(ENet)
+print("\nLasso score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+fit_model_and_submission(lasso, 'lasso')
+fit_model_and_submission(ENet, 'ENet')
+fit_model_and_submission(KRR, 'KRR')
+fit_model_and_submission(Boost, 'Boost')
+fit_model_and_submission(model_xgb, 'xgb')
+fit_model_and_submission(model_lgb, 'lgb')
+
+#Simple average model:
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+        
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+        
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+    
+    #Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)   
+    
+averaged_models = AveragingModels(models = (ENet, Boost, KRR, lasso))
+fit_model_and_submission(averaged_models, 'simpleAVG')
+
+
+
+#Stacking average model class
+
+base_model = [ENet, Boost, KRR,model_xgb]
+#base_model = [model_xgb]
+meta_model = clone(lasso)
+n_fold = 5
+
+base_models_ = [list() for x in base_model]
+kfold = KFold(n_splits= n_folds, shuffle=True, random_state=156)
+out_of_fold_predictions = np.zeros((train.shape[0], len(base_model)))
+
+for i, model in enumerate(base_model):
+    for train_index, holdout_index in kfold.split(train.values, y_train):
+        instance = clone(model)
+        base_models_[i].append(instance)
+        instance.fit(train.values[train_index], y_train[train_index])
+        y_pred = instance.predict(train.values[holdout_index])
+        out_of_fold_predictions[holdout_index, i] = y_pred
+                
+      
+meta_model.fit(out_of_fold_predictions, y_train)
+
+
+def stacked_model_prediction(X):
+#Prediction of Stacking average model
+#Avergae prediction results of models of 5 folds
+#For each models, it derive 5 sub models which are trained by different folds
+    meta_feature = np.column_stack([np.column_stack([model.predict(X.values) for model in base_models]).mean(axis=1)
+                for base_models in base_models_ ])
+    
+    return meta_model.predict(meta_feature)
+
+
+
+#pack stacked model into class for fit with other module
+    
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+   
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        
+        X = X.values
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+        
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+                
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+   
+    #Do the predictions of all base models on the test data and use the averaged predictions as 
+    #meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X.values) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_ ])
+        return self.meta_model_.predict(meta_features)
+
+stacked_averaged_models = StackingAveragedModels(base_models = ([ENet, Boost, KRR,model_xgb]),
+                                                 meta_model = lasso)
+
+fit_model_and_submission(stacked_averaged_models, 'stackAVG')
+
+#Esemble XGBoost, LightGBM, stacked_averaged_models
+
+stacked_averaged_models_predict = stacked_averaged_models.predict(train)
+model_xgb_predict = model_xgb.predict(train)
+model_lgb_predict = model_lgb.predict(train)
+ens_predict =  stacked_averaged_models_predict*0.70 + model_xgb_predict*0.15 + model_lgb_predict*0.15 
+print('Stacked RMSE: {}'.format(rmsle(y_train,stacked_averaged_models_predict)))
+print('XGBoost RMSE: {}'.format(rmsle(y_train,model_xgb_predict)))
+print('Ensemble RMSE: {}'.format(rmsle(y_train,ens_predict)))
+
+stacked_averaged_models_predict = stacked_averaged_models.predict(test)
+model_xgb_predict = model_xgb.predict(test)
+ens_predict =  stacked_averaged_models_predict*0.70 + model_xgb_predict*0.15 + model_xgb_predict*0.15 
+submission(ens_predict, 'Ensemble')
 
 
 
